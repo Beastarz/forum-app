c24c2a431f3f788c7f24c35a1859460c5c31d4ac
Beastarz <jygoh1453@gmail.com>
2025-01-21 21:49:34 +0800

    Update CORS configuration

--- backend/main.go
+++ backend/main.go
@@ -31,7 +31,7 @@ func main() {
 	//set up CORS
 	app.Use(logger.New())
 	app.Use(cors.New(cors.Config{
-		AllowOrigins: "http://localhost:5173",
+		AllowOrigins: "yappers101.netlify.app",
 		AllowHeaders: "Origin, Content-Type, Accept, Authorization",
 		AllowMethods: "GET, POST, PUT, DELETE",
 	}))
